b5e22fa78e6284fa3afb978610ad0732f7067522
NancyDrewn <nanacynje98@gmail.com>
2024-07-17 02:19:15 -0700

    first commit

--- drrawer/lib/main.dart
+++ drrawer/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mydrawer/screens/home_screen.dart';
+import 'package:drrawer/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
